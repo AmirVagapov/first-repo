9e32f93b8a01ac724860ce0eb2109e598e0b5aa3
Amirka <Vagapovamir@yandex.ru>
2017-09-26 21:28:36 +0300

    Интерфейс чата

---
+++ Chat/ChatInterface.java
@@ -0,0 +1,17 @@
+package java_level_two.lesson_seven;
+
+import java.awt.*;
+
+/**
+ * Created by Tom on 10.09.2017.
+ */
+public interface ChatInterface {
+    int POS_X = 500;
+    int POS_Y = 200;
+    int HEIGHT_WINDOW = 500;
+    int WIDTH_WINDOW = 450;
+    Font font = new Font("Times new roman", Font.BOLD, 13);
+    String YOU = "You: ";
+    String AUTH_INVITATION = "Press the button \"Authorized/ Create user\"";
+
+}
